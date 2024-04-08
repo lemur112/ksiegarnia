0bcefd16866c0aa1223c01301edecb1a47450577
lemur112 <klimczak.bartosz@icloud.com>
2024-04-08 14:06:33 +0200

    Dodaj pliki projektów.

---
+++ JsonModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ksiegarnia
+{
+    internal class JsonModel
+    {
+        public required int ID { get; set; }
+        public required string tytul { get; set; }
+        public required string Autor { get; set; }
+        public required DateTime RokWydania { get; set; }
+        public required string Gatunek { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection.Metadata;
+using System.Transactions;
+
+namespace ksiegarnia
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                string json = File.ReadAllText("ksiazki.json");
+            }catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
